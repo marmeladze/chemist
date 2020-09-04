f5363f79bb8488cef77e4e20cb895081350e3e3a
Ziya Bayramov <ziya.bayramov.1@yandex.com>
2020-09-04 19:43:55 +0400

    add urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('companies/<int:id>/drugs', views.drugs_by_company, name='drugs_by_company')
     # path('companies/', views.companies_list, name='companies_listing'),
 ]
